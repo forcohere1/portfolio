ce97740c883b11b7c163a5cd441ab55799d49e30
forcohere1 <forcohere1@hotmail.com>
2024-09-02 13:40:41 +0000

    Your commit message

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,15 +10,14 @@ module.exports = {
       gray: '#818181',
       'light-gray': '#D9D9D9',
       black: '#181818',
-      primary: '#0059B2', // blue
-      // primary: '#007FFF', // light blue
-      // primary: '#E76F00', // orange
+      primary: '#d22d77', // pink
+  
       white: '#FFFFFF',
       transparent: 'transparent',
     },
     extend: {
       backgroundImage: () => ({
-        'hero-pattern': 'url("/assets-hero.svg")',
+        // 'hero-pattern': 'url("/assets-hero.svg")',
       }),
     },
   },
