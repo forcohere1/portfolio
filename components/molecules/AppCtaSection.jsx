4beef326de38ddb06820dd66c352fe9cf5272d9f
forcohere1 <forcohere1@hotmail.com>
2024-09-04 10:42:49 +0000

    updates

--- components/molecules/AppCtaSection.jsx
+++ components/molecules/AppCtaSection.jsx
@@ -3,9 +3,9 @@ import AppButton from '../atomics/AppButton';
 const AppCtaSection = () => (
   <section className="py-20 border-b border-light-gray">
     <div className="text-center">
-      <h2 className="text-3xl lg:text-4xl font-bold text-black">
-        Let's Connect
-      </h2>
+    <h2 className="text-3xl lg:text-4xl font-bold text-black">
+      Let&apos;s Connect
+    </h2>
       <p className="text-xs md:text-sm text-gray max-w-[260px] md:max-w-xs mx-auto mt-2">
       I'm always here to help. Feel free to reach out via email or social media anytime. 
       Thank you for visiting!
