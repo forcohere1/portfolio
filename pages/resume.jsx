a23d4b9e2b349c75622ecf3b0d057ea253a86501
forcohere1 <forcohere1@hotmail.com>
2024-09-04 01:52:53 +0000

    changed

--- pages/resume.jsx
+++ pages/resume.jsx
@@ -3,6 +3,7 @@ import AppHeader from '../components/organisms/AppHeader';
 import AppSection from '../components/molecules/AppSection';
 import AppTimeline from '../components/atomics/AppTimeline';
 import AppShell from '../components/templates/AppShell';
+import AppButton from '../components/atomics/AppButton';
 
 const Resume = () => (
   <AppShell
@@ -14,6 +15,26 @@ const Resume = () => (
       title={USER.resume.title}
       description={USER.resume.description}
     />
+    
+    {/* Centered PDF Resume Button */}
+    <div className="flex justify-center my-8">
+      <a
+        href="https://firebasestorage.googleapis.com/v0/b/portfolio-daf77.appspot.com/o/Vijay_Chandar_CV.pdf?alt=media&token=068b81bf-cf4c-45ec-8f9e-aade810f6a99"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="inline-block w-max p-2 mt-6 border border-light-gray"
+      >
+        <span
+          className="inline-block min-w-[200px] text-xs lg:text-sm text-center font-bold py-3 px-9 bg-primary hover:bg-black text-white hover:text-white transition duration-300"
+        >
+          View & Download PDF Resume
+        </span>
+      </a>
+    </div>
+
+
+
+
     {USER.resume.contents.map((item) => (
       <AppSection key={item.id} title={item.title} className="py-0">
         {item.contents.map((subItem) => (
