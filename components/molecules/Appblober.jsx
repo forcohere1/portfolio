99795c49747015848da16b477a23ea4bb1ebafbb
forcohere1 <forcohere1@hotmail.com>
2024-09-05 02:38:40 +0000

    changes

--- components/molecules/Appblober.jsx
+++ components/molecules/Appblober.jsx
@@ -7,6 +7,8 @@ const AppBlober = () => (
       <div className="absolute inset-0 z-10">
         <AppBlob />
       </div>
+      {/* Light greyish mesh/grid on top in dark mode */}
+      <div className="absolute inset-0 z-5 dark:bg-grid-pattern pointer-events-none" />
       {/* Text with lower zIndex to appear below the blob */}
       <div className="absolute inset-0 flex flex-col justify-center items-center z-0">
         <h1 className="text-3xl font-bold text-black dark:text-dark-black text-center md:text-4xl">Empowering AI</h1>
